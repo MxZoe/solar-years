9d9310cef0153ea3799a3d907a51bfe47e9d6f05
Zoe Weinstein <mxzweinstein@gmail.com>
2021-12-24 18:27:30 -0700

    updated beforeeach variable

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -1,10 +1,18 @@
 import Year from './../src/js/years.js'
 
 describe('Year', ()=>{
+  const testYear;
+  beforeEach(()=>{
+    testYear = new Year(31, 80);
+  });
 
   test('should create a Year object with age and lifeExpectancy attributes', ()=>{
-    const testYear = new Year(31, 80);
+    
     expect(testYear.age).toEqual(31);
     expect(testYear.lifeExpectancy).toEqual(80);
   })
+
+  test('should return the mercury equivelant of the user\'s age'){
+
+  }
 })
